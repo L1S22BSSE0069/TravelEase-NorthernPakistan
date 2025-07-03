5a9f343bb55c5fb590d1ac13ac25bf7944eb0a88
L1S22BSSE0130 <L1S22BSSE0130@ucp.edu.pk>
2025-07-04 01:13:50 +0500

    Add Stylelint config with 10 rules

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+    "declaration-block-no-duplicate-properties": true,
+    "declaration-block-no-shorthand-property-overrides": true,
+    "font-family-no-duplicate-names": true,
+    "no-descending-specificity": true,
+    "property-no-unknown": true,
+    "selector-pseudo-class-no-unknown": true,
+    "selector-type-no-unknown": true,
+    "unit-no-unknown": true
+  }
+};
